f32232ff3d4ed31a306ad2d5b4e7fbe740a70f58
lizzz <20071724@mail.wit.ie>
2023-02-11 21:04:19 +0000

    Model complete!

---
+++ app/src/main/java/ie/setu/pumpitup/models/PumpModel.kt
@@ -0,0 +1,9 @@
+package ie.setu.pumpitup.models
+
+data class PumpModel(var station_name: String = "",
+                     var petrol_price: String = "",
+                     var diesel_price: String = "",
+                     var eircode: String = "",
+                     var address: String = ""
+
+                                                    )
