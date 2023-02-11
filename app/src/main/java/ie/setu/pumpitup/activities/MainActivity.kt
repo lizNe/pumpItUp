d32b8c01b574d7dabd11cf773ad793b0a0f71b15
lizzz <20071724@mail.wit.ie>
2023-02-11 20:49:12 +0000

    Packages activities,adapters,main and model added

--- app/src/main/java/ie/setu/pumpitup/activities/MainActivity.kt
+++ app/src/main/java/ie/setu/pumpitup/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package ie.setu.pumpitup
+package ie.setu.pumpitup.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ie.setu.pumpitup.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
